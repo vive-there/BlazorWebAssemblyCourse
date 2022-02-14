fa6d966dc79e147531cd48bbc70d6163405264f7
Vadym Tarasov <Vadym_Tarasov@epam.com>
2022-02-14 14:08:19 +0200

    updates

---
+++ BlazorProducts.Client/Components/Home.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Components
+{
+    public partial class Home
+    {
+        [Parameter]
+        public string? Title { get; set; }
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object>? AdditionalAttributes { get; set; }
+
+        [CascadingParameter(Name = "HeadingColor")]
+        public string? Color { get; set; }
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; } //ChildContent is required name of property to render fragment
+
+        [Parameter]
+        public RenderFragment? VisitShopChildContent { get; set; } 
+
+
+    }
+}
